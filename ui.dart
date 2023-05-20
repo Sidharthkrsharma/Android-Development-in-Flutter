0a82163f649986fd8983534faf42fb26e42e659f
Sidharth Kumar <95740490+Sidharthkrsharma@users.noreply.github.com>
2023-05-20 19:02:56 +0530

    Update and rename ui.txt to ui.dart

--- ui.dart
+++ ui.dart
@@ -1,10 +1,9 @@
-1.A= align-items property sets the align-self value on all direct children as a group. In Flexbox, it controls the alignment of items on the Cross Axis. In Grid Layout, it controls the alignment of items on the Block Axis
+//1.A= align-items property sets the align-self value on all direct children as a group. In Flexbox, it controls the alignment of items on the Cross Axis. In Grid Layout, it controls the alignment of items on the Block Axis
 
-1.B= Row and Column are the two most important and powerful widgets in Flutter. These widgets let you align children horizontally and vertically as per the requirement. As we know that when we design any UI(User Interface)  in a flutter, we need to arrange its content in the Row and Column manner so these Row and Column widgets are required when designing UI. 
-mainAxisAlignment: This property takes in MainAxisAlignment enum as the object to decide how the children widgets should be place in mainAxisAlignment. For Row it is horizontal and for Column it is vertical.
+//1.B= Row and Column are the two most important and powerful widgets in Flutter. These widgets let you align children horizontally and vertically as per the requirement. As we know that when we design any UI(User Interface)  in a flutter, we need to arrange its content in the Row and Column manner so these Row and Column widgets are required when designing UI. 
+//mainAxisAlignment: This property takes in MainAxisAlignment enum as the object to decide how the children widgets should be place in mainAxisAlignment. For Row it is horizontal and for Column it is vertical.
 
-
-2.import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class ConversationList extends StatefulWidget{
   String name;
